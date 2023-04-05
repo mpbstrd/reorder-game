5b6acbfd0afb6c3f075bdd2da66c5a1570bbe6c3
matt <matthewperrybustarde@gmail.com>
2023-04-05 13:45:32 +0800

    added country_pop,  country_area, and limited the items to maximum of 10

--- components/SortableList.vue
+++ components/SortableList.vue
@@ -1,6 +1,6 @@
 <template>
     <ul>
-        <li v-for="(item, index) in items" 
+        <li v-for="(item, index) in itemLimit" 
         :key="item.id" 
         :draggable="true" 
         @dragstart="dragStart(index)" 
@@ -13,26 +13,49 @@
   
 <script>
 export default {
-props: {
-    items: {
-    type: Array,
-    required: true
-    }
-},
-methods: {
-    dragStart(index) {
-    this.draggedItemIndex = index;
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
     },
-    drop(index) {
-    const draggedItem = this.items.splice(this.draggedItemIndex, 1)[0];
-    this.items.splice(index, 0, draggedItem);
+    computed: {
+        itemLimit() {
+            const maxItems = 10;
+            const arr = [];
+            for (let i = 0; i < maxItems && i < this.items.length; i++) {
+                arr.push(this.items[i]);
+            }
+            return arr;
+        }
+    },
+    methods: {
+        dragStart(index) {
+            this.draggedItemIndex = index;
+        },
+        drop(index) {
+            const draggedItem = this.items.splice(this.draggedItemIndex, 1)[0];
+            this.items.splice(index, 0, draggedItem);
+        }
+    },
+    data() {
+        return {
+            draggedItemIndex: null
+        };
     }
-},
-data() {
-    return {
-    draggedItemIndex: null
-    };
-}
 };
 </script>
   
+<style>
+ul {
+    list-style: none;
+    padding: 0;
+}
+li {
+    padding: 10px;
+    margin-bottom: 5px;
+    background-color: #eee;
+    cursor: pointer;
+    width: 50%;
+}
+</style>
