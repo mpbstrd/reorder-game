6950271c746b35eb0b52ce875dd56fb35e6a64ab
matt <matthewperrybustarde@gmail.com>
2023-04-05 13:06:17 +0800

    better approach at sortable list

---
+++ components/SortableList.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul>
+        <li v-for="(item, index) in items" 
+        :key="item.id" 
+        :draggable="true" 
+        @dragstart="dragStart(index)" 
+        @dragover.prevent @drop="drop(index)"
+        >
+        {{ item.name }}
+        </li>
+    </ul>
+</template>
+  
+<script>
+export default {
+props: {
+    items: {
+    type: Array,
+    required: true
+    }
+},
+methods: {
+    dragStart(index) {
+    this.draggedItemIndex = index;
+    },
+    drop(index) {
+    const draggedItem = this.items.splice(this.draggedItemIndex, 1)[0];
+    this.items.splice(index, 0, draggedItem);
+    }
+},
+data() {
+    return {
+    draggedItemIndex: null
+    };
+}
+};
+</script>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,28 @@
 <template>
   <div id="app">
-    <main class="flexbox">
-      <Board id="board-1">
-        <Card id="card-1" draggable="true">
-          <p>Card 1</p>
-        </Card>
-        <Card id="card-2" draggable="true">
-          <p>Card 2</p>
-        </Card>
-        <Card id="card-3" draggable="true">
-          <p>Card 3</p>
-        </Card>
-        <Card id="card-4" draggable="true">
-          <p>Card 4</p>
-        </Card>
-      </Board>
-    </main>
+    <h1>Sortable List</h1>
+    <SortableList :items="items" />
   </div>
 </template>
 
 <script>
-import Board from '@/components/Board.vue'
-import Card from '@/components/Card.vue'
+import SortableList from '@/components/SortableList.vue';
+
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    Board,
-    Card
+    SortableList
   },
+  data() {
+    return {
+      items: [
+        { id: 1, name: 'Item 1' },
+        { id: 2, name: 'Item 2' },
+        { id: 3, name: 'Item 3' },
+        { id: 4, name: 'Item 4' },
+        { id: 5, name: 'Item 5' },
+      ]
+    }
+  }
 }
 </script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  background-color: #f3f3f3;
-}
-
-.flexbox {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  max-width: 768px;
-  height: 100vh;
-  overflow: hidden;
-  margin: 0 auto;
-  padding: 15px;
-}
-
-.flexbox .board {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  max-width: 300px;
-  background-color: #313131;
-  padding: 15px;
-}
-
-.flexbox .board .card {
-  padding: 15px 25px;
-  background-color: #f3f3f3;
-  cursor: pointer;
-  margin-bottom: 15px;
-}
-</style>
